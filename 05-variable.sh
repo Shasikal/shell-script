5a9f1d8f692939595e833faad2d51d7167f4b095
shasikala <shasikala27linux@gmail.com>
2024-01-31 11:56:24 +0530

    through promt

--- 05-variable.sh
+++ 05-variable.sh
@@ -2,8 +2,8 @@
 
 echo "enter the username: "
 read USERNAME
-echo "username entered is: $USERNAME
+echo "username entered is: $USERNAME"
 
 echo "enter the password: "
 read -s PASSWORD
-echo "the password enterd is : $PASSWORD
+echo "the password enterd is : $PASSWORD"
