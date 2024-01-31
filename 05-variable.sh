f6213a9eb020804b2f91bfa36f2c19f1b27d4c31
shasikala <shasikala27linux@gmail.com>
2024-01-31 11:31:38 +0530

    through promt

---
+++ 05-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "enter the username: "
+read USERNAME
+echo "username entered is: $USERNAME
+
+echo :enter the password: 
+read -s PASSWORD
+echo "the password enterd is : $PASSWORD
